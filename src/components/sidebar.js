35d46a13fa8124047659bada0f0774f6dbab681f
Blake Ingenthron <blakeingenthron@gmail.com>
2022-03-11 17:36:57 -0600

    added layout functionality

---
+++ src/components/sidebar.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export default function Sidebar() {
+  return (
+    <nav>
+      <input placeholder="Search..." />
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </nav>
+  )
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,10 @@
 import Layout from '../components/layout'
 import Sidebar from '../components/sidebar'
 
-export default function Index() {
+export default function About() {
   return (
     <section>
-      <h2>Layout Example (Index)</h2>
+      <h2>Layout Example (About)</h2>
       <p>
         This example adds a property <code>getLayout</code> to your page,
         allowing you to return a React component for the layout. This allows you
@@ -13,7 +13,7 @@ export default function Index() {
       </p>
       <p>
         When navigating between pages, we want to persist page state (input
-        values, scroll position, etc) for a Single-Page Application (SPA)
+        values, scroll position, etc.) for a Single-Page Application (SPA)
         experience.
       </p>
       <p>
@@ -32,7 +32,7 @@ export default function Index() {
   )
 }
 
-Index.getLayout = function getLayout(page) {
+About.getLayout = function getLayout(page) {
   return (
     <Layout>
       <Sidebar />
