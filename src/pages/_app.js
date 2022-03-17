6bd6dfe7cdb5812fa23ad8b57d00dd1ed44fd71e
Blake Ingenthron <blakeingenthron@gmail.com>
2022-03-17 18:49:42 -0500

    testing fit function

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,5 +10,21 @@ export default function MyApp({ Component, pageProps }) {
     document.body.classList?.remove('loading')
   }, [])
 
+  useEffect(() => {
+    function calculateVh() {
+      var vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', vh + 'px')
+    }
+
+    // Initial calculation
+    calculateVh()
+
+    // Re-calculate on resize
+    window.addEventListener('resize', calculateVh)
+
+    // Re-calculate on device orientation change
+    window.addEventListener('orientationchange', calculateVh)
+  }, [])
+
   return getLayout(<Component {...pageProps} />)
 }
