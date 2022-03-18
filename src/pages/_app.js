255b4f85d7b7ecce12473ca9f7004267ed9bd2f8
Blake Ingenthron <blakeingenthron@gmail.com>
2022-03-18 13:11:51 -0500

    better?

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,26 +1,37 @@
 import '../styles/main.css'
 import '../styles/chrome-bug.css'
 import { useEffect } from 'react'
+import useWindowDimensions from 'src/components/useWIndowDimensions'
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
+  const { height } = useWindowDimensions()
+
+  const vh = height * 0.01
+
+  // useEffect(() => {
+  //   function calculateVh() {
+  //     var vh = window.innerHeight * 0.01
+  //     document.documentElement.style.setProperty('--vh', vh + 'px')
+  //   }
+
+  //   // Initial calculation
+  //   calculateVh()
+
+  //   // Re-calculate on resize
+  //   window.addEventListener('resize', calculateVh)
+
+  //   // Re-calculate on device orientation change
+  //   window.addEventListener('orientationchange', calculateVh)
+  // })
+
   useEffect(() => {
-    function calculateVh() {
-      var vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty('--vh', vh + 'px')
-    }
+    document.documentElement.style.setProperty('--vh', vh + 'px')
+  }, [vh])
 
-    // Initial calculation
-    calculateVh()
-
-    // Re-calculate on resize
-    window.addEventListener('resize', calculateVh)
-
-    // Re-calculate on device orientation change
-    window.addEventListener('orientationchange', calculateVh)
-
+  useEffect(() => {
     document.body.classList?.remove('loading')
   }, [])
 
